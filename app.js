5e8688b4b2316767a6ce4d4b8efd11f5a56df57a
yoavz <yoavzimmerman@gmail.com>
2014-01-25 11:41:16 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,53 @@
+
+/**
+ * Module dependencies
+ */
+
+var express = require('express'),
+  routes = require('./routes'),
+  api = require('./routes/api'),
+  http = require('http'),
+  path = require('path'),
+  engines = require('consolidate');
+
+
+var app = module.exports = express();
+
+
+
+/**
+* Configuration
+*/
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.engine('html', engines.mustache);
+app.set('view engine', 'html');
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(app.router);
+
+// development only
+if (app.get('env') === 'development') {
+   app.use(express.errorHandler());
+};
+
+// production only
+if (app.get('env') === 'production') {
+  // TODO
+}; 
+
+
+// redirect all routes to index
+app.get('*', routes.index);
+
+/**
+* Start Server
+*/
+
+http.createServer(app).listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
+});
