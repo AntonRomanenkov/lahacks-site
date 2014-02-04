633806857426a03e932bc526cea018d2df426990
yoavz <yoavzimmerman@gmail.com>
2014-02-03 16:27:12 -0800

    deleted stuff

--- blue/js/jquery.custom.js
+++ blue/js/jquery.custom.js
@@ -243,46 +243,6 @@
 			fixedContentPos: false
 		});
 		
-		
-		/* Flexslider with Video
-		================================================== */
-		
-		/*$(window).load(function(){
-			
-			// Vimeo API nonsense
-		  	var player = document.getElementById('player_1');
-		  	$f(player).addEvent('ready', ready);
-		  
-			function addEvent(element, eventName, callback) {
-				(element.addEventListener) ? element.addEventListener(eventName, callback, false) : element.attachEvent(eventName, callback, false);
-			}
-			
-			function ready(player_id) {
-				var froogaloop = $f(player_id);
-			  
-				froogaloop.addEvent('play', function(data) {
-				  $('.flexslider').flexslider("pause");
-				});
-				
-				froogaloop.addEvent('pause', function(data) {
-				  $('.flexslider').flexslider("play");
-				});
-			}
-			
-			$('.flexslider').fitVids().flexslider({
-				animation: "slide",
-				controlNav: "thumbnails",
-				smoothHeight: true,
-				start: function(slider){
-			  		$('body').removeClass('loading');
-				},
-				before: function(slider){
-					$f(player).api('pause');
-			  	}
-		  	});
-			
-		}); */
-		
 		/* Flexslider without Video
 		================================================== */
 		$(window).load(function(){
