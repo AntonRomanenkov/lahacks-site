b6e639c6fe15132314cf1f012613e4385edd0807
yoavz <yoavzimmerman@gmail.com>
2014-02-04 21:01:34 -0800

    check for empty email

--- routes/api.js
+++ routes/api.js
@@ -3,15 +3,19 @@
  */
 var mcapi = require('mailchimp-api');
 var mc = new mcapi.Mailchimp(process.env.MAILCHIMP_API_KEY);
-var list_id = process.env.LIST_ID
+var list_id = process.env.LIST_ID;
 
 exports.subscribe = function(req, res){
-  mc.lists.subscribe({id: list_id, email:{email:req.body.email}}, function(data) {
-      res.contentType('json');
-      res.send({ some: JSON.stringify({response:'success'}) });
-    },
-    function(error) {
-      res.contentType('json');
-      res.send({ some: JSON.stringify({response:error.error}) });
-    });
+  if (req.body && req.body.email) {
+	  mc.lists.subscribe({id: list_id, email:{email:req.body.email}}, function(data) {
+	      res.contentType('json');
+	      res.send({response:'success'});
+	    },
+	    function(error) {
+	      res.contentType('json');
+	      res.send({response:error.error});
+	  });
+  } else {
+    res.send({response:'could not find email'});
+  }
 };
